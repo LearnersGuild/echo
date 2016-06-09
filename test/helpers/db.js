e94dc318655c4a89a8337a241104ec7af1b8a6f8
Trevor Little <trevor@learnersguild.org>
2016-06-09 10:52:06 -0400

    adding instrumentation for debugging timeouts

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -1,12 +1,36 @@
 import r from '../../db/connect'
 
-function truncateDBTables() {
-  return r.tableList()
-    .then(tables => tables.filter(t => !t.startsWith('_')))
-    .then(tablesToTruncate => Promise.all(tablesToTruncate.map(t => r.table(t).delete().run())))
+// const sleep3 = () => new Promise(resolved => {
+//   console.log('sleep start')
+//   return setTimeout(() => {
+//     console.log('sleep stopped')
+//     resolved()
+//   }, 3000)
+// })
+
+async function truncateDBTables() {
+  const start = new Date().getTime()
+  const tables = await r.tableList()
+  const tablesToTruncate = tables.filter(t => !t.startsWith('_'))
+  await Promise.all(tablesToTruncate.map(async t => {
+    try {
+      const tableStart = new Date().getTime()
+      await r.table(t).delete()
+      const tableEnd = new Date().getTime()
+      console.log(`truncated ${t} in ${tableEnd - tableStart} ms`)
+    } catch (e) {
+      throw (e)
+    }
+  }))
+  const now = new Date().getTime()
+  console.log(`truncated all tables in ${now - start} ms`)
 }
 
 /* eslint-env mocha */
+/* eslint-disable prefer-arrow-callback */
 export function withDBCleanup() {
-  beforeEach(truncateDBTables)
+  beforeEach(function () {
+    this.timeout(10000)
+    return truncateDBTables()
+  })
 }
