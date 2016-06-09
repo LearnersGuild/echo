0d936b5097f6fe2da250a0fab716ea82ba466bf7
Trevor Little <trevor@learnersguild.org>
2016-06-09 11:12:48 -0400

    comment out profiling. set timeout to 21 seconds =(

--- test/helpers/db.js
+++ test/helpers/db.js
@@ -1,28 +1,32 @@
 import r from '../../db/connect'
 
 async function truncateDBTables() {
-  const start = new Date().getTime()
-  const tables = await r.tableList()
-  const tablesToTruncate = tables.filter(t => !t.startsWith('_'))
-  await Promise.all(tablesToTruncate.map(async t => {
-    try {
-      const tableStart = new Date().getTime()
-      await r.table(t).delete()
-      const tableEnd = new Date().getTime()
-      console.log(`truncated ${t} in ${tableEnd - tableStart} ms`)
-    } catch (e) {
-      throw (e)
-    }
-  }))
-  const now = new Date().getTime()
-  console.log(`truncated all tables in ${now - start} ms`)
+  try {
+    // const start = new Date().getTime()
+    const tables = await r.tableList()
+    const tablesToTruncate = tables.filter(t => !t.startsWith('_'))
+    await Promise.all(tablesToTruncate.map(async t => {
+      try {
+        // const tableStart = new Date().getTime()
+        await r.table(t).delete()
+        // const tableEnd = new Date().getTime()
+        // console.log(`truncated ${t} in ${tableEnd - tableStart} ms`)
+      } catch (e) {
+        throw (e)
+      }
+    }))
+    // const now = new Date().getTime()
+    // console.log(`truncated all tables in ${now - start} ms`)
+  } catch (e) {
+    throw (e)
+  }
 }
 
 /* eslint-env mocha */
 /* eslint-disable prefer-arrow-callback */
 export function withDBCleanup() {
   beforeEach(function () {
-    // this.timeout(10000)
+    this.timeout(21000)
     return truncateDBTables()
   })
 }
