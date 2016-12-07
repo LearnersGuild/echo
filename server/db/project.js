3331bc2158cd6350c5d38058cc80269f41696b3e
SJ Pratt <pratt.sabra@gmail.com>
2016-12-07 08:35:18 -0800

    minor function rename

--- server/db/project.js
+++ server/db/project.js
@@ -32,8 +32,8 @@ export function getProjectsForChapter(chapterId) {
   return table.getAll(chapterId, {index: 'chapterId'})
 }
 
-export function getProjectsForPlayer(playerId) {
-  return findProjects(project => (project('playerIds').contains(playerId)))
+export function findProjectsForUser(userId) {
+  return findProjects(project => (project('playerIds').contains(userId)))
 }
 
 export function findProjectsByIds(projectIds) {
