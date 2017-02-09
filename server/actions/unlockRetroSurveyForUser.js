1659defa18544ca141f440f4e57ae57c74ec3f19
pllearns@gmail.com <philliplorenzo@Phillips-MacBook-Pro.local>
2017-02-09 14:21:15 -0800

    WIP Created new files to handle unlocking logic

--- server/actions/unlockRetroSurveyForUser.js
+++ server/actions/unlockRetroSurveyForUser.js
@@ -1,9 +1,9 @@
 import {Project, Survey} from 'src/server/services/dataService'
 
 export async function unlockRetroSurveyForUser(playerId, projectId) {
-<<<<<<< HEAD
   // const project = await Project.get(projectId)
   // const survey = await project.surveyModel()
+
   // survey.unlockedFor.push(playerId)
   // await survey.save()
 }
@@ -11,33 +11,7 @@ export async function unlockRetroSurveyForUser(playerId, projectId) {
 export async function lockRetroSurveyForUser(playerId, projectId) {
   // const project = await Project.get(projectId)
   // const survey = await project.surveyModel()
+
   // survey.unlockedFor = // remove playerId
   // await survey.save()
-=======
-  const surveyId = await _getCompletedRetroId(playerId, projectId)
-
-  await Survey.get(surveyId).update(s => ({
-    unlockedFor: s('unlockedFor').default([]).add([playerId]).distinct()
-  }))
-}
-
-export async function lockRetroSurveyForUser(playerId, projectId) {
-  const surveyId = await _getCompletedRetroId(playerId, projectId)
-
-  await Survey.get(surveyId).update(s => ({
-    unlockedFor: s('unlockedFor').default([]).filter(id => id.ne(playerId))
-  }))
-}
-
-async function _getCompletedRetroId(playerId, projectId) {
-  const project = await Project.get(projectId).getJoin({retrospectiveSurvey: true})
-  _assertSurveyIsCompleted(project.retrospectiveSurvey, playerId)
-  return project.retrospectiveSurveyId
-}
-
-function _assertSurveyIsCompleted(survey, playerId) {
-  if (!survey.completedBy.includes(playerId)) {
-    throw new Error('Cannot toggle lock for an incomplete survey')
-  }
->>>>>>> WIP Completed tests, backend methods, and first api endpoint for unlock/lock of surveys
 }
