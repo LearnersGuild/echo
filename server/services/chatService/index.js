1e84497ef07ab847a486aefd4176abdb9aa03ea7
pllearns <pllearns@gmail.com>
2017-03-28 14:11:28 -0700

    WIP: refactored index file to fix error in testing

--- server/services/chatService/index.js
+++ server/services/chatService/index.js
@@ -21,13 +21,13 @@
 //   '/api/im.open'          {token, user} --> {ok, channel:{id}}
 // }
 
-export {default as createChannel} from './createChannel'
-
 /**
  * NOTE: this service's functions are exported the way they are to enable
  * certain stubbing functionality functionality for testing that relies on the
  * way the module is cached and later required by dependent modules.
  */
+
+
 
 export {default as createChannel} from './createChannel'
 export {default as createChannelMessage} from './createChannelMessage'
