9ba9c6f3d18aeb82d44cac543ba90a7d4064c77c
Devon Wesley <dwesley89@gmail.com>
2017-04-18 11:34:42 -0700

    changes need for front end work Amelia is handling. for findProjectsThatNeedReview

--- server/actions/findProjectsToReview.js
+++ server/actions/findProjectsToReview.js
@@ -1,12 +1,16 @@
 import {Project, Player} from 'src/server/services/dataService'
 import {PROJECT_STATES} from 'src/common/models/project'
+
 const {REVIEW} = PROJECT_STATES
 
 export async function findProjectsToReview({coachId}) {
   const {chapterId} = await Player.get(coachId)
+  // can you pass filter an object with two params
   const reviewableProjects = Project.filter({state: REVIEW, chapterId})
-  
+
   const sortByNumExternalReviews = projects => projects
+    // Not sure about this line below. does it work.
+    .filter(project => project('playerIds').contains(coachId).not())
     .getJoin({
       projectReviewSurvey: {
         responses: true
