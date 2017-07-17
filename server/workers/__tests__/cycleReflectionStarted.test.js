a071fe61d0e56b05761485b11d7bd0b4fbbcda33
anasauce <anasauceda1843@gmail.com>
2017-07-17 12:06:21 -0700

    fixing commit history

--- server/workers/__tests__/cycleReflectionStarted.test.js
+++ server/workers/__tests__/cycleReflectionStarted.test.js
@@ -21,44 +21,42 @@ describe.only(testContext(__filename), function () {
     const {processCycleReflectionStarted} = require('../cycleReflectionStarted')
 
     describe('when reflection has started', function () {
-      describe('for phases having retrospective', function () {
-        beforeEach('setup data & mocks', async function () {
-          useFixture.nockClean()
-          this.cycle = await factory.create('cycle', {state: 'PRACTICE'})
-          this.phaseWithRetrospective = await factory.create('phase', {
-            hasRetrospective: true,
-            channelName: '#phase-channel-4'
-          })
-          this.phaseNoRetrospective = await factory.create('phase', {
-            hasRetrospective: false,
-            channelName: '#phase-channel-2'
-          })
-          this.projectWithRetrospective = await factory.createMany('project', {
-            phaseId: this.phaseWithRetrospective.id,
-            cycleId: this.cycle.id,
-          }, 3)
-          this.projectNoRetrospective = await factory.createMany('project', {
-            phaseId: this.phaseNoRetrospective.id,
-            cycleId: this.cycle.id,
-          }, 3)
-          this.usersWithRetrospective = await mockIdmUsersById(this.projectWithRetrospective.memberIds)
+      beforeEach('setup data & mocks', async function () {
+        useFixture.nockClean()
+        this.cycle = await factory.create('cycle', {state: 'PRACTICE'})
+        this.phaseWithRetrospective = await factory.create('phase', {
+          hasRetrospective: true,
+          channelName: '#phase-channel-4'
         })
-
-        it('sends the cycle reflection announcement in direct message to phase project members', async function () {
-          const memberHandles = this.usersWithRetrospective.map(u => u.handle)
-          await processCycleReflectionStarted(this.cycle)
-          expect(chatService.sendDirectMessage).to.have.been
-            .calledWithMatch(memberHandles, 'Time to start your reflection process for cycle')
+        this.phaseNoRetrospective = await factory.create('phase', {
+          hasRetrospective: false,
+          channelName: '#phase-channel-2'
         })
-
-        it('sends the cycle reflection announcement in phase channel', async function () {
-          await processCycleReflectionStarted(this.cycle)
-          expect(chatService.sendChannelMessage).to.have.been
-            .calledWithMatch(this.phaseWithRetrospective.channelName, 'Time to start your reflection process for cycle')
-        })
-
-        it('does not send a cycle reflection announcement to ')
+        this.projectWithRetrospective = await factory.createMany('project', {
+          phaseId: this.phaseWithRetrospective.id,
+          cycleId: this.cycle.id,
+        }, 3)
+        this.projectNoRetrospective = await factory.createMany('project', {
+          phaseId: this.phaseNoRetrospective.id,
+          cycleId: this.cycle.id,
+        }, 3)
+        this.usersWithRetrospective = await mockIdmUsersById(this.projectWithRetrospective.memberIds)
       })
+
+      it('sends the cycle reflection announcement in direct message to phase project members', async function () {
+        const memberHandles = this.usersWithRetrospective.map(u => u.handle)
+        await processCycleReflectionStarted(this.cycle)
+        expect(chatService.sendDirectMessage).to.have.been
+          .calledWithMatch(memberHandles, 'Time to start your reflection process for cycle')
+      })
+
+      it('sends the cycle reflection announcement in phase channel', async function () {
+        await processCycleReflectionStarted(this.cycle)
+        expect(chatService.sendChannelMessage).to.have.been
+          .calledWithMatch(this.phaseWithRetrospective.channelName, 'Time to start your reflection process for cycle')
+      })
+
+      it('does not send a cycle reflection announcement to ph')
     })
   })
 })
