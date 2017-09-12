79ee7278fd032a7e12545d4a57bb4b16a907aaf3
Serafin <leaf466@gmail.com>
2017-09-11 17:21:59 -0700

    Attempts (and fails) to correct linting error: 'Cycle not found in src/server/services/dataService' WIP: Moving on - come back to this commit

--- src/server/services/dataService/index.js
+++ src/server/services/dataService/index.js
@@ -4,6 +4,19 @@ import queries from './queries'
 
 export default {
   r,
-  ...models,
+  modelDefinitions: models.modelDefinitions,
+  chapter: models.chapter,
+  cycle: models.cycle,
+  feedbackType: models.feedbackType,
+  member: models.member,
+  phase: models.phase,
+  pool: models.pool,
+  poolMember: models.poolMember,
+  project: models.project,
+  question: models.question,
+  response: models.response,
+  survey: models.survey,
+  surveyBlueprint: models.surveyBlueprint,
+  vote: models.vote,
   ...queries,
 }
